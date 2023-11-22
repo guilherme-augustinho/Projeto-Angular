d3b25380283ebbf6f14d796f3d854baf2bca2984
Guilherme Proença <guilhermemorais06072005@gmail.com>
2023-11-22 15:13:21 -0300

    Inicido do Back e Front

---
+++ BackEnd/Model/GuinaLanchesContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Model;
+
+public partial class GuinaLanchesContext : DbContext
+{
+    public GuinaLanchesContext()
+    {
+    }
+
+    public GuinaLanchesContext(DbContextOptions<GuinaLanchesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=SJP-C-00002\\SQLEXPRESS01;Initial Catalog=GuinaLanches;Integrated Security=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Cliente__3214EC27DF3ADD01");
+
+            entity.ToTable("Cliente");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.DataNasc).HasColumnType("date");
+            entity.Property(e => e.Nome)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Senha)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
